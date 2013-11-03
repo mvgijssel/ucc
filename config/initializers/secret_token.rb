f89e656bc1f1d41ce1b3ff799a53f870ff2fbdae
Maarten van Gijssel <maarten@vgijssel.nl>
2013-11-03 03:16:17 +0100

    Installed ruby
    Summary: Updated Rubymine files
    
    Test Plan: - Check if the installation is successful
    
    Reviewers: Luuk
    
    Reviewed By: Luuk
    
    Differential Revision: http://phab.wmc2.nl/D21

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waddenzee::Application.config.secret_token = 'f908198d424f5b90d4c139082e951b61cdd15f0c45f613160c26fc7f7bdf73422705cde94435b914b198dfe7d035106c04fff9c25ee1418a223262a1c936a010'
